fb5b7ad0ea6e48611cb61cd808b5dcd8f91dd50c
Justin Monge <justin.mnge@gmail.com>
2024-09-06 05:57:45 -0300

    created window, added audio and image files

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 import pygame
+from constants import *
 
 def main():
     pygame.init()
+   
+    display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    clock = pygame.time.Clock()
+    running = True
+    
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        
+        display_surface.fill("black")
+        
+        pygame.display.flip()
+        
+        clock.tick(60)
+
 
 if __name__ == "__main__":
     main()
