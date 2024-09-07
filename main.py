3e16da7538b54b968d5174411b00669f4ae0d96a
Justin Monge <justin.mnge@gmail.com>
2024-09-07 14:20:33 -0300

    updated main.py. Player spaceship movement within window

--- main.py
+++ main.py
@@ -16,12 +16,21 @@ def main():
     # plain surface
     surf = pygame.Surface((100, 200))
     surf.fill('grey')
-    x = 100
+    x = 590
     
     # importing an image
     player_surface = pygame.image.load(join('images', 'player.png')).convert_alpha()
+    player_rect = player_surface.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+    player_direction = 1
+    
     stars_surface = pygame.image.load(join('images', 'star.png')).convert_alpha()
     star_positions = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for i in range(20)]
+    
+    meteor_surface = pygame.image.load(join('images', 'meteor.png')).convert_alpha()
+    meteor_rect = meteor_surface.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT /2))
+    
+    laser_surface = pygame.image.load(join('images', 'laser.png')).convert_alpha()
+    laser_rect = laser_surface.get_frect(bottomleft = (20, WINDOW_HEIGHT - 20))                                  
             
     while running:
         # event loop
@@ -33,8 +42,15 @@ def main():
         display_surface.fill('black')
         for pos in star_positions:
             display_surface.blit(stars_surface, pos)
-        x += 1.2
-        display_surface.blit(player_surface, (x, 460))
+            
+        display_surface.blit(meteor_surface, meteor_rect)
+        display_surface.blit(laser_surface, laser_rect)            
+        
+        # player movement
+        player_rect.x += player_direction * 2
+        if player_rect.right > WINDOW_WIDTH or player_rect.left < 0:
+            player_direction *= -1
+        display_surface.blit(player_surface, player_rect)
         
         pygame.display.flip()
         
