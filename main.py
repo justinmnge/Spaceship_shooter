7c4c2891b1104f498d5634b520d73b22285675fc
Justin Monge <justin.mnge@gmail.com>
2024-09-08 21:09:39 -0300

    Created player class

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 from os.path import join
 from random import randint
 from constants import *
-
+from player import *
 
 # general setup
 def main():
@@ -17,10 +17,13 @@ def main():
     surf = pygame.Surface((100, 200))
     surf.fill('grey')
     
+    all_sprites = pygame.sprite.Group()
+    player = Player(all_sprites)
+
     # importing an image
-    player_surface = pygame.image.load(join('images', 'player.png')).convert_alpha()
-    player_rect = player_surface.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
-    player_direction = pygame.math.Vector2()
+    # player_surface = pygame.image.load(join('images', 'player.png')).convert_alpha()
+    # player_rect = player_surface.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+    # player_direction = pygame.math.Vector2()
     
     stars_surface = pygame.image.load(join('images', 'star.png')).convert_alpha()
     star_positions = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for i in range(40)]
@@ -39,17 +42,19 @@ def main():
                 running = False
         
         # input
-        keys = pygame.key.get_pressed()
-        player_direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
-        player_direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
-        player_direction = player_direction.normalize() if player_direction else player_direction
-        player_rect.center += player_direction * PLAYER_SPEED * dt
+        # keys = pygame.key.get_pressed()
+        # player_direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
+        # player_direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
+        # player_direction = player_direction.normalize() if player_direction else player_direction
+        # player_rect.center += player_direction * PLAYER_SPEED * dt
         
         # laser
-        recent_keys = pygame.key.get_just_pressed()
-        if recent_keys[pygame.K_SPACE]:
-            print('fired laser')
-        
+        # recent_keys = pygame.key.get_just_pressed()
+        # if recent_keys[pygame.K_SPACE]:
+        #     print('fired laser')
+            
+        all_sprites.update()
+ 
         # draw the game
         display_surface.fill('black')
         for pos in star_positions:
@@ -57,7 +62,9 @@ def main():
             
         display_surface.blit(meteor_surface, meteor_rect)
         display_surface.blit(laser_surface, laser_rect)           
-        display_surface.blit(player_surface, player_rect)
+        # display_surface.blit(player_surface, player_rect)
+        all_sprites.draw(display_surface)
+        
         
         pygame.display.flip()
 
---
+++ player.py
@@ -0,0 +1,12 @@
+import pygame
+from os.path import join
+from constants import *
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, groups):
+        super().__init__(groups)
+        self.image = pygame.image.load(join('images', 'player.png')).convert_alpha()
+        self.rect = self.image.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+        
+    def update(self):
+        print('ship is being updated')
